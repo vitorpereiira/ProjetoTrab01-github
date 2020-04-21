0e3545dcc516d76e267597b0087fc77aa7143e18
Vitor Pereira <vitor.we@hotmail.com>
2020-04-21 17:07:22 -0300

    Termino do codigo contendo, as classes, progam, departamento, contratohora, workerlevel, worker

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,61 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.ContratoHora;
+import entities.Departamento;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.println("Digite o nome do setor do trabalhador: ");
+		String departamentoNome = leia.nextLine();
+		System.out.println("Insira os dados do trabalhador ");
+		System.out.print("Nome: ");
+		String workerName = leia.nextLine();
+		System.out.print("Level do trabalhador: ");
+		String workerLevel = leia.nextLine();
+		System.out.print("Salario base: ");
+		double baseSalary = leia.nextDouble();
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,
+				new Departamento(departamentoNome));
+
+		System.out.println("Quantos contratos tem esse trabalhador ?");
+		int n = leia.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Insira os dados do " + i + " contrato: ");
+			System.out.print("Digite a data (DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(leia.next());
+			System.out.print("Valor por hora: ");
+			double valorPorHora = leia.nextDouble();
+			System.out.print("Duracao (hora): ");
+			int hora = leia.nextInt();
+			ContratoHora contrato = new ContratoHora(contractDate, valorPorHora, hora);
+			worker.addContrato(contrato);
+		}
+		
+		System.out.println();
+		System.out.print("Digite o mes e o ano para calcular o salario do trabalhador (MM/YYYY): ");
+		String monthAndYear = leia.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Nome: " + worker.getNome());
+		System.out.println("Departamento: "+worker.getDepartamento().getNome());
+		System.out.println("Renda para " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+
+		leia.close();
 
 	}
 
---
+++ src/entities/Worker.java
@@ -0,0 +1,85 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	
+	private String nome;
+	private WorkerLevel level;
+	private Double SalarioBase;
+
+	private Departamento departamento;
+	private List<ContratoHora> contratos = new ArrayList<>();
+	
+	public Worker() {
+		}
+
+	public Worker(String nome, WorkerLevel level, Double salarioBase, Departamento departamento) {
+		this.nome = nome;
+		this.level = level;
+		SalarioBase = salarioBase;
+		this.departamento = departamento;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getSalarioBase() {
+		return SalarioBase;
+	}
+
+	public void setSalarioBase(Double salarioBase) {
+		SalarioBase = salarioBase;
+	}
+
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	public List<ContratoHora> getContratos() {
+		return contratos;
+	}
+
+	public void addContrato(ContratoHora contrato) {
+		contratos.add(contrato);
+	}
+	
+	public void removeContrato(ContratoHora contrato) {
+		contratos.remove(contrato);
+	}
+	
+	public double income(int year, int month) {
+		double sum = SalarioBase;
+		Calendar cal = Calendar.getInstance();
+		for (ContratoHora c : contratos) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+}
